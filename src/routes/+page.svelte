0d3c43bd63118a443e234508338c59bfd94cd09e
Christopher Carson <chris@nowzoo.com>
2024-05-05 10:26:36 -0500

    max-wdth

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,17 +14,19 @@
 
 <main class="prose mx-auto mb-96 mt-16 max-w-none px-4 lg:container">
   <h1 class="border-b pb-4">Flaky State Warnings</h1>
-
-  <p>
-    An irrelevant (as far as the state in question goes) modal wrapper results
-    in an <code>ownership_invalid_binding</code> dev warning:
-  </p>
-  <blockquote>
-    [svelte] ownership_invalid_binding.../src/lib/PretendModal.svelte passed a
-    value to .../src/lib/LuckyNumbersControl.svelte with `bind:`, but the value
-    is owned by .../src/routes/EditForm.svelte. Consider creating a binding
-    between .../src/routes/EditForm.svelte and .../src/lib/PretendModal.svelte
-  </blockquote>
+  <div class="max-w-prose">
+    <p>
+      An irrelevant (as far as the state in question goes) modal wrapper results
+      in an <code>ownership_invalid_binding</code> dev warning:
+    </p>
+    <blockquote>
+      [svelte] ownership_invalid_binding.../src/lib/PretendModal.svelte passed a
+      value to .../src/lib/LuckyNumbersControl.svelte with `bind:`, but the value
+      is owned by .../src/routes/EditForm.svelte. Consider creating a binding
+      between .../src/routes/EditForm.svelte and .../src/lib/PretendModal.svelte
+    </blockquote>
+  </div>
+  
 
   <div class="grid gap-y-4 md:grid-cols-2 md:gap-x-8">
     <div>
